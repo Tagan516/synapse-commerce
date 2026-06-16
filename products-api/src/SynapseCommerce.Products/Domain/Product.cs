fe0603f3bbfc649b0e4c364e3a6bafc8759be5f1
Tagan Williamson <93137070+Tagan516@users.noreply.github.com>
2026-06-15 21:49:52 -0400

    Add Product class and ProductDbContext

---
+++ products-api/src/SynapseCommerce.Products/Domain/Product.cs
@@ -0,0 +1,16 @@
+namespace SynapseCommerce.Products.Domain
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsAvailable { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
