02ff1c8c2cd8b07fede72328baf9d8819258d675
Tagan Williamson <93137070+Tagan516@users.noreply.github.com>
2026-06-20 15:26:16 -0400

    Added the create product endpoint and create product request DTO, added comments, and updated the product db context naming

--- products-api/src/SynapseCommerce.Products/Domain/Product.cs
+++ products-api/src/SynapseCommerce.Products/Domain/Product.cs
@@ -2,15 +2,15 @@ namespace SynapseCommerce.Products.Domain
 {
     public class Product
     {
-        public Guid Id { get; set; }
-        public required string SKU { get; set; }
-        public required string Name { get; set; }
-        public required string Description { get; set; }
-        public decimal Price { get; set; }
-        public bool IsActive { get; set; }
-        public bool IsAvailable { get; set; }
-        public required string ImageUrl { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public Guid Id { get; set; } // System defined
+        public required string SKU { get; set; } // User defined
+        public required string Name { get; set; } // User defined
+        public required string Description { get; set; } //User defined
+        public decimal Price { get; set; } //User defined
+        public bool IsActive { get; set; } //System defined
+        public bool IsAvailable { get; set; } //System defined
+        public required string ImageUrl { get; set; } //System defined, DTO takes raw image
+        public DateTime CreatedAt { get; set; } //System defined
+        public DateTime UpdatedAt { get; set; } //System defined
     }
 }
--- products-api/src/SynapseCommerce.Products/Program.cs
+++ products-api/src/SynapseCommerce.Products/Program.cs
@@ -2,15 +2,19 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Get the connection string from config
 var connectionString = builder.Configuration.GetConnectionString("ProductsDbConnectionString");
+// Get the password for the db conn string
 var dbPassword = builder.Configuration["DbConnectionPassword"];
+// Build the PosgreSQL conn string and insert the password
 var npgsqlBuilder = new Npgsql.NpgsqlConnectionStringBuilder(connectionString)
 {
     Password = dbPassword
 };
-
-builder.Services.AddDbContext<ProductsDBContext>(options =>
+// Add the DbContext with the db conn string
+builder.Services.AddDbContext<ProductsDbContext>(options =>
     options.UseNpgsql(npgsqlBuilder.ConnectionString));
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
