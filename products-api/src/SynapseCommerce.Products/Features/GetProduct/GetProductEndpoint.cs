063c52c6ac744038355c723d1be95cd133a9e17e
Tagan Williamson <93137070+Tagan516@users.noreply.github.com>
2026-06-20 15:52:37 -0400

    Create get product endpoint

---
+++ products-api/src/SynapseCommerce.Products/Features/GetProduct/GetProductEndpoint.cs
@@ -0,0 +1,23 @@
+using SynapseCommerce.Shared;
+
+namespace SynapseCommerce.Products.Features.GetProduct;
+
+public class GetProductEndpoint(ProductsDbContext db) : IEndpoint
+{
+    public void MapRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id:guid}", async (Guid id) =>
+        {
+            var product = await db.Products.FindAsync(id);
+
+            if (product is null)
+            {
+                return Results.NotFound(new { Message = $"Product with ID {id} was not found." });
+            }
+
+            return Results.Ok(product);
+        })
+        .WithTags("Products")
+        .WithName("GetProductById");
+    }
+}
